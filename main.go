2e959e9714108ed58d3fbf57bc8f3429e5b9f88b
najibean <najibullah89@gmail.com>
2021-07-14 03:29:53 +0700

    after migrations from gitlab

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"go-postgres/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+    r := router.Router()
+    // fs := http.FileServer(http.Dir("build"))
+    // http.Handle("/", fs)
+    fmt.Println("Starting server on the port 9090...")
+
+    log.Fatal(http.ListenAndServe(":9090", r))
+}
